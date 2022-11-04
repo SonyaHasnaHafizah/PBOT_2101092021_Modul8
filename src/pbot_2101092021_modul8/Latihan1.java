9f2e727e5a199b338e7791e5ab90f01686023956
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2022-11-04 12:22:32 +0700

    Initial commit

---
+++ src/pbot_2101092021_modul8/Latihan1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbot_2101092021_modul8;
+
+/**
+ *
+ * @author SONYA HASNA HAFIZAH
+ */
+public class Latihan1 {
+    public static void main( String[] args ){
+        int i;
+        
+        for(i=1; i<args.length; i++ )
+        { 
+            System.out.println(args[i]);
+        }
+        
+    }
+}
---
+++ src/pbot_2101092021_modul8/PBOT_2101092021_Modul8.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbot_2101092021_modul8;
+
+/**
+ *
+ * @author SONYA HASNA HAFIZAH
+ */
+public class PBOT_2101092021_Modul8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
